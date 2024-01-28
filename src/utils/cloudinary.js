f84dc1044f1187d1f7775526851ce1eb71ad376b
diptanshu1044 <diptanshubanerjee123@gmail.com>
2024-01-28 11:20:41 +0530

    Video and img data handlers added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    //Checks if the file actually exists
+    if (!localFilePath) return null
+
+    //uploads to the cloud
+    const res = await cloudinary.uploader.upload(localFilePath,
+      { resource_type: "auto" })
+    console.log('File Succesfully uploaded', res)
+    return res
+
+  } catch (err) {
+    //removes the file from the server
+    fs.unlinkSync(localFilePath)
+    console.log(`Cloudinary file upload error: ${err}`);
+    return null
+  }
+}
+
+export { uploadOnCloudinary };
